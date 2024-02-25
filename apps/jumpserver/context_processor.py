3674097b20489d82d3a9408c734868f914bb50e1
thshah2090 <160426637+thshah2090@users.noreply.github.com>
2024-02-26 00:49:31 +0500

    Update context_processor.py

--- apps/jumpserver/context_processor.py
+++ apps/jumpserver/context_processor.py
@@ -9,8 +9,8 @@ default_interface = dict((
     ('logo_index', static('img/logo_text_white.png')),
     ('login_image', static('img/login_image.png')),
     ('favicon', static('img/facio.ico')),
-    ('login_title', _('JumpServer Open Source Bastion Host')),
-    ('theme', 'classic_green'),
+    ('login_title', _('Beyond Connect Bastion Host')),
+    ('theme', 'classic_blue'),
     ('theme_info', {}),
     ('beian_link', ''),
     ('beian_text', '')
@@ -22,7 +22,7 @@ default_context = {
     'LOGIN_WECOM_logo_logout': static('img/login_wecom_logo.png'),
     'LOGIN_DINGTALK_logo_logout': static('img/login_dingtalk_logo.png'),
     'LOGIN_FEISHU_logo_logout': static('img/login_feishu_logo.png'),
-    'COPYRIGHT': 'FIT2CLOUD 飞致云' + ' © 2014-2023',
+    'COPYRIGHT': 'Beyond Connect' + ' © 2014-2023',
     'INTERFACE': default_interface,
 }
 
