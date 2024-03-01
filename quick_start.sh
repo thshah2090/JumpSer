366c364fd5ee013a5667a3de7ce4af661a85623d
thshah2090 <160426637+thshah2090@users.noreply.github.com>
2024-03-02 02:20:44 +0500

    this is try for auto install

---
+++ quick_start.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+#
+
+VERSION=v3.10.3
+DOWNLOAD_URL=https://github.com
+
+function install_soft() {
+    if command -v dnf > /dev/null; then
+      dnf -q -y install "$1"
+    elif command -v yum > /dev/null; then
+      yum -q -y install "$1"
+    elif command -v apt > /dev/null; then
+      apt-get -qqy install "$1"
+    elif command -v zypper > /dev/null; then
+      zypper -q -n install "$1"
+    elif command -v apk > /dev/null; then
+      apk add -q "$1"
+      command -v gettext >/dev/null || {
+      apk add -q gettext-dev python3
+    }
+    else
+      echo -e "[\033[31m ERROR \033[0m] $1 command not found, Please install it first"
+      exit 1
+    fi
+}
+
+function prepare_install() {
+  for i in curl wget tar iptables; do
+    command -v $i &>/dev/null || install_soft $i
+  done
+}
+
+function get_installer() {
+  echo "download install script to /opt/jumpserver-installer-${VERSION}"
+  cd /opt || exit 1
+  if [ ! -d "/opt/jumpserver-installer-${VERSION}" ]; then
+    timeout 60 wget -qO jumpserver-installer-${VERSION}.tar.gz ${DOWNLOAD_URL}/itusman6/jumpserver-tahir/releases/download/jump-server-beta/jumpserver-installer-${VERSION}.tar.gz || {
+      rm -f /opt/jumpserver-installer-${VERSION}.tar.gz
+      echo -e "[\033[31m ERROR \033[0m] Failed to download jumpserver-installer-${VERSION}"
+      exit 1
+    }
+    tar -xf /opt/jumpserver-installer-${VERSION}.tar.gz -C /opt || {
+      rm -rf /opt/jumpserver-installer-${VERSION}
+      echo -e "[\033[31m ERROR \033[0m] Failed to unzip jumpserver-installer-${VERSION}"
+      exit 1
+    }
+    rm -f /opt/jumpserver-installer-${VERSION}.tar.gz
+  fi
+}
+
+function config_installer() {
+  cd /opt/jumpserver-installer-${VERSION} || exit 1
+  sed -i "s/VERSION=.*/VERSION=${VERSION}/g" /opt/jumpserver-installer-${VERSION}/static.env
+  ./jmsctl.sh install
+  ./jmsctl.sh start
+}
+
+function main(){
+  if [[ "${OS}" == 'Darwin' ]]; then
+    echo
+    echo "Unsupported Operating System Error"
+    exit 1
+  fi
+  prepare_install
+  get_installer
+  config_installer
+}
+
+main
